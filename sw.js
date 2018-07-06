715628ed2353190564ff97bd862e638c350bebd1
awesid <siddharthawesome001@gmail.com>
2018-07-06 12:05:08 +0530

    Manifest Added

--- sw.js
+++ sw.js
@@ -25,6 +25,8 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request, {ignoreSearch:true}).then(response => {
       return response || fetch(event.request);
+    }).catch((err)=>{
+      console.log(err);
     })
   );
 });
